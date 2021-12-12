5c433329b0425d4307a546be4e7bfcc2e232d025
RaduTheMan <radu.damian07@gmail.com>
2021-12-12 20:27:26 +0200

    little refactoring

--- create-csv-for-db/src/main/java/com/org/example/Main.java
+++ create-csv-for-db/src/main/java/com/org/example/Main.java
@@ -1,23 +1,10 @@
 package com.org.example;
 
-import com.github.javafaker.*;
-
-import java.util.ArrayList;
-
 public class Main {
 
-    public final static int NR_USERS = 1000;
-
     public static void main(String[] args){
-        Faker faker = new Faker();
-        var users = new ArrayList<User>();
-        for(int i = 0; i < NR_USERS; ++i) {
-            String username = faker.name().username();
-            String email = username + "@yahoo.com";
-            String password = faker.random().hex();
-            User user = new User(username, email, password);
-            users.add(user);
-        }
+        var generator = new UsersGenerator();
+        var users = generator.generate();
         DataProvider provider = new DataProvider(users);
     }
 }
---
+++ create-csv-for-db/src/main/java/com/org/example/UsersGenerator.java
@@ -0,0 +1,28 @@
+package com.org.example;
+
+import com.github.javafaker.Faker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersGenerator {
+
+    public final static int NR_USERS = 1000;
+    private Faker faker;
+
+    UsersGenerator(){
+        this.faker = new Faker();
+    }
+
+    public List<User> generate() {
+        var users = new ArrayList<User>();
+        for(int i = 0; i < NR_USERS; ++i) {
+            String username = faker.name().username();
+            String email = username + "@yahoo.com";
+            String password = faker.random().hex();
+            User user = new User(username, email, password);
+            users.add(user);
+        }
+        return users;
+    }
+}
