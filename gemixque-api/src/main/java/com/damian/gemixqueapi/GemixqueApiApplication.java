9166debaa6fb0edab634ee08dda5422a05c4d36f
RaduTheMan <radu.damian07@gmail.com>
2022-03-11 00:13:13 +0200

    added api project files

---
+++ gemixque-api/src/main/java/com/damian/gemixqueapi/GemixqueApiApplication.java
@@ -0,0 +1,13 @@
+package com.damian.gemixqueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemixqueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemixqueApiApplication.class, args);
+	}
+
+}
---
+++ gemixque-api/src/test/java/com/damian/gemixqueapi/GemixqueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damian.gemixqueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GemixqueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
