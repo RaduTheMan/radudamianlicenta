5dbc5280b7c4a7ee7675b4f4fde884754bb52bd1
RaduTheMan <radu.damian07@gmail.com>
2022-05-03 09:39:57 +0300

    started to add validators for sign up form

--- gemixque-ui/src/app/material/material.module.ts
+++ gemixque-ui/src/app/material/material.module.ts
@@ -4,9 +4,18 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
+const materials = [
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatTooltipModule
+]
 @NgModule({
-  imports: [MatButtonModule, MatFormFieldModule, MatInputModule],
-  exports: [MatButtonModule, MatFormFieldModule, MatInputModule]
+  imports: materials,
+  exports: materials
 })
 export class MaterialModule { }
--- gemixque-ui/src/app/pages/sign-up/sign-up.component.ts
+++ gemixque-ui/src/app/pages/sign-up/sign-up.component.ts
@@ -1,29 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { passwordPattern, usernamePattern } from 'src/app/shared/validators';
 
+@UntilDestroy()
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent {
 
   formGroup: FormGroup;
+  passwordInfo = "The password should have minimum 8 characters, at least one letter and one number";
+  usernameInfo = "The username should have between 8-20 characters, no _ or . at the beginning/ending, and no __ _. ._ or .. inside";
+  reEnterPasswordInfo = "Re-enter your password";
+  emailInfo = "Your email address";
+
+  get password(): AbstractControl | null {
+    return this.formGroup?.get('password');
+  }
+
+  get controls() {
+    return this.formGroup.controls;
+  }
 
   constructor() {
     this.formGroup = new FormGroup({
-      username: new FormControl(null, [Validators.required]),
+      username: new FormControl(null, [Validators.required, Validators.pattern(usernamePattern)]),
       email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [Validators.required]),
-      repeatedPassword: new FormControl(null, [Validators.required])
+      password: new FormControl(null, [Validators.required, Validators.pattern(passwordPattern)]),
+      repeatedPassword: new FormControl(null, [Validators.required, Validators.pattern(passwordPattern), this.mustMatchPassword.bind(this)])
     });
    }
 
-  ngOnInit() {
-  }
-
   signUp(): void {
     console.log(this.formGroup.getRawValue());
+  }
+
+  mustMatchPassword(control: FormControl): {[s: string]: boolean} | null {
+    if(this.password?.value !== control.value) {
+      return { 'notMatch': true };
+    }
+    return null;
   }
 
 }
